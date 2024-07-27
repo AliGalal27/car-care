85f676ff055281a9ce8eada35e332ef38daccd8e
Ali <aligalalhassan27@gmail.com>
2024-07-27 21:45:09 +0300

    Initial commit

---
+++ src/main/java/org/example/carcare/services/ServicesService.java
@@ -0,0 +1,64 @@
+package org.example.carcare.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicesService {
+
+    private final ServicesRepository servicesRepository;
+
+    @Autowired
+    public ServicesService(ServicesRepository servicesRepository) {
+        this.servicesRepository = servicesRepository;
+    }
+
+
+    public List<Services> getServices(){
+        return servicesRepository.findAll();
+    }
+
+    public void addNewService(Services service) {
+        Optional<Services> serviceOptional = servicesRepository.findByServiceid(service.getService_id());
+        if (serviceOptional.isPresent()) {
+            throw new IllegalStateException("Service already exists");
+        }
+        else {
+            servicesRepository.save(service);
+            System.out.println("service added");
+        }
+    }
+    public void deleteService(int id) {
+        Optional<Services> serviceOptional = servicesRepository.findById(id);
+        if (serviceOptional.isEmpty()) {
+            throw new IllegalStateException("Service not found");
+        }
+        else {
+            servicesRepository.deleteById(id);
+        }
+    }
+    @Transactional
+    public Services updateService(
+            int serviceId, String serviceName, String description, String serviceType, int price) {
+        Optional<Services> optionalService = servicesRepository.findById(serviceId);
+        if (optionalService.isPresent()) {
+            Services existingService = optionalService.get();
+            if (serviceName != null) existingService.setService_name(serviceName);
+            if (description != null) existingService.setDescription(description);
+            if (serviceType != null) existingService.setServiceType(serviceType);
+            existingService.setPrice(price);
+            return servicesRepository.save(existingService);
+        } else {
+            throw new RuntimeException("Service not found with id: " + serviceId);
+        }
+    }
+
+    public List<Services> getServicesByType(String serviceType) {
+        return servicesRepository.findByServiceType(serviceType);
+    }
+}
+
