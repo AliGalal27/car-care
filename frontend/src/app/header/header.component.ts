c68f30768593384f491b218d270faef979341c2b
BasharMenissy <127782782+BasharMenissy@users.noreply.github.com>
2024-07-30 23:38:24 +0300

    Add files via upload

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  goToHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  goToServices(): void {
+    this.router.navigate(['/services']);
+  }
+
+  goToContact(): void {
+    this.router.navigate(['/contact']);
+  }
+
+  goToReview(): void {
+    this.router.navigate(['/review']);
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  goToSignup(): void {
+    this.router.navigate(['/signup']);
+  }
+}
